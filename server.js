c5c023c52fd73913e36c660a2fe5e3237c24f104
Samuel-Deyvid-0ficial <127963039+Samuel-Deyvid-0ficial@users.noreply.github.com>
2026-02-23 00:05:47 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (data) => {
+        io.emit('chat message', data);
+    });
+});
+
+// Essencial para o Render funcionar
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
